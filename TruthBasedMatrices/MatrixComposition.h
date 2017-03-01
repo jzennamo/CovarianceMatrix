c53a2f64b94f9121995ef3cb80566725256ea2dc
Andy Mastbaum <amastbaum@gmail.com>
2017-03-01 15:00:41 -0600

    refactor the matrix calculator
    * Factored out sample-level stuff into a helper class, to support more
      than two samples
    * Added calculations of the n-sample correlations
    * Use a universe-wise product of user-specified weights (currently this
      list is just hard-coded, will move to a config file)

--- TruthBasedMatrices/MatrixComposition.h
+++ TruthBasedMatrices/MatrixComposition.h
@@ -1,3 +1,6 @@
+#ifndef LARLITE_MATRIXCOMPOSITION_H
+#define LARLITE_MATRIXCOMPOSITION_H
+
 /**
  * \file MatrixComposition.h
  *
@@ -5,84 +8,114 @@
  * 
  * \brief Class def header for a class MatrixComposition
  *
- * @author jzennamo
+ * \author mastbaum@uchicago.edu, jzennamo
  */
 
 /** \addtogroup CrossSectionMatrix
 
     @{*/
 
-#ifndef LARLITE_MATRIXCOMPOSITION_H
-#define LARLITE_MATRIXCOMPOSITION_H
-
 #include "Analysis/ana_base.h"
-#include "DataFormat/mcpart.h"
-#include "DataFormat/mctruth.h"
-#include "DataFormat/mctrack.h"
-#include "DataFormat/mcshower.h"
-#include "DataFormat/mceventweight.h"
+#include <set>
+#include <string>
+#include <vector>
 
-#include "TGraph.h"
-#include "TH1D.h"
-#include "TH2D.h"
-
+class TGraphErrors;
+class TH1D;
+class TH2D;
 
 namespace larlite {
   /**
-     \class MatrixComposition
-     User custom analysis class made by SHELL_USER_NAME
+   * \class MatrixComposition
    */
-  class MatrixComposition : public ana_base{
+  class MatrixComposition : public ana_base {
   
   public:
 
-    /// Default constructor
-    MatrixComposition(){ _name="MatrixComposition"; _fout=0;}
+    class EventSample {
+      public:
+        /**
+         * Constructor.
+         *
+         * Note: You can optionally specify the number of systematics
+         * universes up front with the nweights parameter. If this isn't
+         * known until runtime, call Resize() later.
+         *
+         * \param _name String name for the event sample
+         * \param nbins Number of energy bins
+         * \param elo Lower limit of energy spectrum
+         * \param ehi Upper limit of energy spectrum
+         * \param nweights Number of systematics universes (see note)
+         */
+        EventSample(std::string _name="sample", size_t nbins=14,
+                    double elo=0.2, double ehi=3.0, size_t nweights=0);
 
-    /// Default destructor
-    virtual ~MatrixComposition(){}
+        /** Destructor. */
+        ~EventSample();
 
-    /** IMPLEMENT in MatrixComposition.cc!
-        Initialization method to be called before the analysis event loop.
-    */ 
+        /**
+         * Get the energy spectrum as a graph with error bars representing
+         * the systematic uncertainty.
+         */
+        TGraphErrors* EnuCollapsed();
+
+        /** Set the number of universes. */
+        void Resize(size_t nweights);
+
+        /**
+         * Covariance Matrix
+         *
+	 *   i && j     = energy bins
+	 *   n          = number of weights
+	 *   N^cv_i     = number of events in bin i for the central value
+	 *   N^syst_i,m = number of events in bin i for the systematic
+	 *                variation in universe "m"
+	 *   E_ij       = the covariance (square of the uncertainty) for
+	 *                bins i,j
+	 *   E_ij = (1/n) Sum((N^cv_i - N^syst_i,m)*(N^cv_j - N^syst_j,m), m)
+	 */
+        static TH2D* CovarianceMatrix(TH1D* nom, std::vector<TH1D*> syst);
+
+        /** Covariance matrix using internal histograms */
+        TH2D* CovarianceMatrix();
+
+        /** Correlation matrix: Corr[ij] = Cov[ij]/Sqrt(Cov[ii]*Cov[jj]) */
+        static TH2D* CorrelationMatrix(TH2D* _cov);
+
+        /** Correlation matrix using internal histograms */
+        TH2D* CorrelationMatrix();
+
+        std::string name;  //!< String name for this event sample
+        TH1D* enu;  //!< "Nominal" energy spectrum
+        std::vector<TH1D*> enu_syst;  //!< Spectra for each systematic universe
+
+      protected:
+        TH2D* cov;  //!< Cached covariance matrix
+    };
+
+    /** Default constructor */
+    MatrixComposition() { _name = "MatrixComposition"; _fout = 0; }
+
+    /** Default destructor */
+    virtual ~MatrixComposition() {}
+
+    /** Initialization method to be called before the analysis event loop. */
     virtual bool initialize();
 
-    /** IMPLEMENT in MatrixComposition.cc! 
-        Analyze a data event-by-event  
-    */
+    /** Analyze data event-by-event */
     virtual bool analyze(storage_manager* storage);
 
-    /** IMPLEMENT in MatrixComposition.cc! 
-        Finalize method to be called after all events processed.
-    */
+    /** Finalize method to be called after all events processed. */
     virtual bool finalize();
 
-
-    TH1D* weight_nue; 
-    TH1D* weight_numu;
-
-    TH1D* Enu_nue; 
-    std::vector<TH1D*> syst_Enu_nue;
-
-    TH1D* Enu_numu; 
-    std::vector<TH1D*> syst_Enu_numu;
-
-    TH2D* cov_nue;
-    TH2D* cov_numu;
-    TH2D* cov_numunue;
-
-    TH2D* corr_nue;
-    TH2D* corr_numu;
-    TH2D* corr_numunue;
-
-    TH2D* weights_v_energy_numu;
-
-
   protected:
-    
+    std::set<std::string> use_weights;  //!< Weight functions to use
+    std::vector<EventSample*> samples;  //!< Event samples
   };
-}
-#endif
+
+}  // namespace larlite
+
+#endif  // LARLITE_MATRIXCOMPOSITION_H
 
 //**************************************************************************
 // 
@@ -93,3 +126,4 @@ namespace larlite {
 //**************************************************************************
 
 /** @} */ // end of doxygen group 
+
